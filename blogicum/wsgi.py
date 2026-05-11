98f3ebdda9404c955499c8a31688146900c57446
yandexidinakfsdljavbkd <nikmach.li@gmail.com>
2026-05-11 18:21:25 +0300

    Add files via upload

---
+++ blogicum/wsgi.py
@@ -0,0 +1,8 @@
+"""WSGI config for blogicum project."""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogicum.settings')
+
+application = get_wsgi_application()

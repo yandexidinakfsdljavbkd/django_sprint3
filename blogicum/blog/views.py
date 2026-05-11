8411fd9a73966c436f8fa33bab2c90aada9cfe76
yandexidinakfsdljavbkd <nikmach.li@gmail.com>
2026-05-11 18:48:57 +0300

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,48 +1,20 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер.''',
-    },
-]
-
-posts_by_id = {post['id']: post for post in posts}
+from .models import Category, Post
 
 
 def index(request):
     template = 'blog/index.html'
 
+    post_list = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )
+
     context = {
-        'post_list': posts[::-1]
+        'post_list': post_list,
     }
 
     return render(request, template, context)
@@ -51,13 +23,16 @@ def index(request):
 def post_detail(request, post_id):
     template = 'blog/detail.html'
 
-    post = posts_by_id.get(post_id)
-
-    if post is None:
-        raise Http404(f'Пост с id={post_id} не найден')
+    post = get_object_or_404(
+        Post,
+        pk=post_id,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )
 
     context = {
-        'post': post
+        'post': post,
     }
 
     return render(request, template, context)
@@ -66,13 +41,21 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     template = 'blog/category.html'
 
-    post_list = [
-        post for post in posts
-        if post['category'] == category_slug
-    ]
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True,
+    )
+
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )
 
     context = {
-        'category_slug': category_slug,
+        'category': category,
         'post_list': post_list,
     }
 
